95db7512e1ae448a52e85860b67b0fc7fce3855a
CraserF <57032835+CraserF@users.noreply.github.com>
2022-12-03 14:15:51 +0200

    Display the results in a sliver on load

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import '../services/f1_data_api.dart';
 import 'package:flutter/material.dart';
+import '../classes/standings_results.dart';
 
 /// Stateful home page of the application.
 class MyHomePage extends StatefulWidget {
@@ -18,17 +20,45 @@ class _MyHomePageState extends State<MyHomePage> {
         // Use the passed title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
+      body: FutureBuilder<List<Standings>>(
+        future: standingsPerYear(),
+        initialData: const [],
+        builder: (context, snapshot) {
+          /// Display on error.
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                'Something went wrong. Please try reloading/restarting! ${snapshot.error}',
+                textAlign: TextAlign.center,
+              ),
+            );
+          }
+
+          /// Display while loading.
+          if (!snapshot.hasData) {
+            return const Center(
+              child: SizedBox(
+                height: 150,
+                width: 150,
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+
+          /// Scroll View for allowing sliver to dynamically load content.
+          return CustomScrollView(
+            slivers: [
+              /// Sliver list for dynamic displaying of content.
+              SliverList(
+                delegate: SliverChildListDelegate(
+                  snapshot.data!
+                      .map((e) => Text(e.driver?.fullName ?? ''))
+                      .toList(),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
