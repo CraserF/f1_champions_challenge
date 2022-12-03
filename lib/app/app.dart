9c737a2f6bddabe9636e04d9ccc279f45cc083f1
CraserF <57032835+CraserF@users.noreply.github.com>
2022-12-03 12:20:39 +0200

    Added comments

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -9,10 +9,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // The Material App to use.
     return MaterialApp(
-      title: 'Flutter Demo',
+      // The app title
+      title: 'F1 Champions',
+      // Light and dark themes
       theme: ThemeData(useMaterial3: true, colorScheme: lightColorScheme),
       darkTheme: ThemeData(useMaterial3: true, colorScheme: darkColorScheme),
+      // The root page.
       home: const MyHomePage(title: 'Home Page'),
     );
   }
