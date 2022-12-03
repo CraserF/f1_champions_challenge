55814f885e2c00540af88f2a3b6b1eaf5b1be9cb
CraserF <57032835+CraserF@users.noreply.github.com>
2022-12-03 12:20:28 +0200

    Added comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 
+/// Main Function for starting the app.
 void main() {
+  // Initialize the Material App.
   runApp(const MyApp());
 }
